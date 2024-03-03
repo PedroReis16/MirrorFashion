7040761ca1e60b3bec21f5f25b2992453ff17709
Pedro Henrique <pedro_henrique_dsr@hotmail.com>
2024-03-03 16:35:15 -0300

    Separando o carrosel em arquivos js

--- script/product.js
+++ script/product.js
@@ -1,3 +1,5 @@
+import { Carousel } from "./model/product_carousel.js";
+
 document.addEventListener("DOMContentLoaded", function () {
     const newsList = [
         { image: "/images/produtos/foto5-azul.png", description: "Regata azul", value: 19.99 },
@@ -9,76 +11,11 @@ document.addEventListener("DOMContentLoaded", function () {
         { image: "/images/produtos/foto10-rosa.png", description: "Camisa manga longa", value: 69.99 },
     ];
 
-    function criarCard(item) {
-        var card = document.createElement("div");
-        card.className = "card";
-    
-        var image = document.createElement("img");
-        image.src = item.image;
-        image.alt = item.description;
-        image.className = "cardImage";
-        card.appendChild(image);
-    
-        var description = document.createElement("p");
-        description.className = "productDescription";
-        description.textContent = item.description;
-        card.appendChild(description);
-    
-        var productValue = document.createElement("p");
-        productValue.className = "productValue";
-        productValue.textContent = `R$ ${item.value.toFixed(2)}`;
-        card.appendChild(productValue);
-    
-        return card;
-    }
-    
-    function updateCarousel() {
-        const cards = document.querySelectorAll(".card");
-        const transitionDuration = 1000;
-    
-        cards.forEach(card => {
-            card.style.transition = `transform ${transitionDuration}ms ease-in-out`;
-            card.style.transform = "translateX(-100%)";
-        });
-    
-        setTimeout(() => {
-            cards.forEach(card => carouselContent.removeChild(card));
-    
-            for (let i = currentIndex; i < currentIndex + totalItems; i++) {
-                const index = i % totalItems;
-                const card = criarCard(newsList[index]);
-                carouselContent.appendChild(card);
-            }
-    
-            setTimeout(() => {
-                cards.forEach(card => {
-                    card.style.transition = "none";
-                    card.style.transform = "translateX(0)";
-                });
-            }, 0);
-        }, transitionDuration);
-    }
-    
-    let currentIndex = 0;
-    const carouselContent = document.getElementById("newsSlide");
-    const totalItems = newsList.length;
-    
-    function nextSlide() {
-        currentIndex = (currentIndex + 1) % totalItems;
-        updateCarousel();
-    }
-    
-    function prevSlide() {
-        currentIndex = (currentIndex - 1 + totalItems) % totalItems;
-        updateCarousel();
-    }
-    
+    const newsCarousel = new Carousel("newsSlide", newsList);
+
     const nextButton = document.getElementById("nextNewsButton");
     const prevButton = document.getElementById("prevNewsButton");
-    
-    nextButton.addEventListener('click', nextSlide);
-    prevButton.addEventListener('click', prevSlide);
-    
-    // Inicialização
-    updateCarousel();
+
+    nextButton.addEventListener("click", () => newsCarousel.nextSlide());
+    prevButton.addEventListener("click", () => newsCarousel.prevSlide());
 });
