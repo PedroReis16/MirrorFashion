202f97d9bbc52d152a2ca170fda44809dc59df7e
PedroReis <pedro_henrique_dsr@hotmail.com>
2024-03-11 20:05:01 -0300

    Acertando o caminho das imagens

--- script/product.js
+++ script/product.js
@@ -2,13 +2,13 @@ import { Carousel } from "./model/product_carousel.js";
 
 document.addEventListener("DOMContentLoaded", function () {
     const newsList = [
-        { image: "/images/produtos/foto5-azul.png", description: "Regata azul", value: 19.99 },
-        { image: "/images/produtos/foto5-rosa.png", description: "Regata rosa", value: 19.99 },
-        { image: "/images/produtos/foto5-verde.png", description: "Regata verde", value: 19.99 },
-        { image: "/images/produtos/foto4-verde.png", description: "Jaqueta de inverno", value: 159.99 },
-        { image: "/images/produtos/foto8-verde.png", description: "Camiseta", value: 59.99 },
-        { image: "/images/produtos/foto9-rosa.png", description: "Camisa junina", value: 49.99 },
-        { image: "/images/produtos/foto10-rosa.png", description: "Camisa manga longa", value: 69.99 },
+        { image: "../images/produtos/foto5-azul.png", description: "Regata azul", value: 19.99 },
+        { image: "../images/produtos/foto5-rosa.png", description: "Regata rosa", value: 19.99 },
+        { image: "../images/produtos/foto5-verde.png", description: "Regata verde", value: 19.99 },
+        { image: "../images/produtos/foto4-verde.png", description: "Jaqueta de inverno", value: 159.99 },
+        { image: "../images/produtos/foto8-verde.png", description: "Camiseta", value: 59.99 },
+        { image: "../images/produtos/foto9-rosa.png", description: "Camisa junina", value: 49.99 },
+        { image: "../images/produtos/foto10-rosa.png", description: "Camisa manga longa", value: 69.99 },
     ];
 
     const newsCarousel = new Carousel("newsSlide", newsList);
